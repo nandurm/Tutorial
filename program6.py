7613e1f343c20a93127875ae4a34cb1ada15714f
Nandu R Manoj <nandurmanoj10@gmail.com>
2025-03-27 11:07:38 +0530

    Delete program6.py

--- program6.py
+++
@@ -1,8 +0,0 @@
-def is_right_triangle(a, b, c):
-    return a**2 + b**2 == c**2
-
-side1 = float(input("Enter first side: "))
-side2 = float(input("Enter second side: "))
-side3 = float(input("Enter third side: "))
-sides = sorted([side1, side2, side3])
-print(is_right_triangle(sides[0], sides[1], sides[2]))
