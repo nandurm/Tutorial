f2d7d9fa5e4a9c1e325de7bb4417186b17427701
Nandu R Manoj <nandurmanoj10@gmail.com>
2025-03-27 12:00:36 +0530

    p7.py

---
+++ Tutorial 2/p7.py
@@ -0,0 +1,7 @@
+#Write a Program to converting all lowercase letters into uppercase
+
+def to_uppercase(s):
+    return s.upper()
+
+input_string = input("Enter a string: ")
+print(to_uppercase(input_string))
