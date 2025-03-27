49314deefafd8118f8effc03d7caa113f8ad3fa9
Nandu R Manoj <nandurmanoj10@gmail.com>
2025-03-27 12:57:30 +0530

    p27.py

--- Tutorial 2/p27.py
+++ Tutorial 2/p27.py
@@ -1,6 +1,6 @@
-"""Write a Python program to read a list of numbers and sort the list in a non
-decreasing order without using any built in functions. Separate function should
- be written to sort the list wherein the name of the list is passed as the parameter."""
+# Write a Python program to read a list of numbers and sort the list in a non
+# decreasing order without using any built in functions. Separate function should
+# be written to sort the list wherein the name of the list is passed as the parameter.
 
 def bubble_sort(lst):
     n = len(lst)
