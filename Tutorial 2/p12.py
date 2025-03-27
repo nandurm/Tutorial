6b0daf3fb60f7a363b9538834475a2221e6780c6
Nandu R Manoj <nandurmanoj10@gmail.com>
2025-03-27 12:29:47 +0530

    p12.py

---
+++ Tutorial 2/p12.py
@@ -0,0 +1,7 @@
+# Write Python script for converting Binary number into decimal number.
+
+def binary_to_decimal(b):
+    return int(b, 2)
+
+binary_number = input("Enter a binary number: ")
+print(binary_to_decimal(binary_number))
